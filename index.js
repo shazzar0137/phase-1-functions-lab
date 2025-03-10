a80bb78184dd7eff65af63c68f8717c6ed7ef878
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-10 11:46:38 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42;
+    return Math.abs(someValue - hqLocation);
+  }
+  console.log(distanceFromHqInBlocks(50))
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+  }
+  console.log(distanceFromHqInBlocks(50))
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+  console.log(distanceTravelledInFeet(34, 38))
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return "cannot travel that far";
+    }
+  }
+  console.log(calculatesFarePrice(34, 38));
+
+  
+  
